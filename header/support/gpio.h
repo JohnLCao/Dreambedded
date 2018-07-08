30e66483b1450d63486e80cc32b319ac68a330a9
tudatn <tudatn@sfu.ca>
2018-07-07 20:34:13 -0700

    gpio.h

--- header/support/gpio.h
+++ header/support/gpio.h
@@ -1,2 +1,28 @@
+#pragma once
+
 #include <string>
 #include <fstream>
+
+#define GPIO_PATH "sys/class/gpio"
+enum GPIO_DIRECTION{ INPUT, OUTPUT };
+enum GPIO_VALUE{ LOW=0, HIGH=1 };
+
+using namespace std;
+
+class GPIO {
+private:
+	int number;
+	string path;
+
+	// functions
+	int exportGPIO();
+	int unexportGPIO();
+
+public:
+	GPIO();
+	GPIO(int gpioNumber);
+	int setDirection(GPIO_DIRECTION);
+	GPIO_DIRECTION getDirection();
+	int setValue(GPIO_VALUE);
+	GPIO_VALUE getValue();
+};
