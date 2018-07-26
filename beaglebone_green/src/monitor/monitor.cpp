13a273c98159878e5996dab4e7f26cfcd591a329
tudatn <tudatn@sfu.ca>
2018-07-26 00:32:17 -0700

    sound_sensor and clapping function (thread)

--- beaglebone_green/src/monitor/monitor.cpp
+++ beaglebone_green/src/monitor/monitor.cpp
@@ -8,19 +8,83 @@
 #include "monitor/monitor.h"
 #include <string>
 #include <iostream>
+#include <thread>
+#include <string>
+#include <cstdlib>
+#include <ratio>
+#include <chrono>
+#include <unistd.h>
 #include "support/network/network.h"
 
 #define POLL_PERIOD 100000
+#define TIME_INTERVAL_ms 500
+#define SOUND_TRIGGER_VALUE 1500
+#define NUM_SAMPLES 10
+#define NUM_SLAPS 2
+#define SOUND_SENSOR_AIN 4
+
+typedef std::chrono::high_resolution_clock Time;
+typedef std::chrono::milliseconds ms;
+typedef std::chrono::duration<float> fsec;
 
 using namespace std;
 
 //XXX add #define _GLIBCXX_USE_CXX11_ABI 0  	--as a stop gap.
 // 		  before including any C++ libraries.
 
+// status for sensors
+int soundRelayActivation = 0;
+
+
+// function for soundSensor thread
+void driveByClappingWithSoundSensor() {
+	auto begin = Time::now();
+	int soundReadValue = 0;
+	int slapCount = 0;
+	int reachTriggerValue = 0;
+
+	SoundSensor soundSensor = SoundSensor(SOUND_SENSOR_AIN);
+	while (1) {
+		slapCount = 0;
+        while (slapCount <= NUM_SLAPS) {
+            reachTriggerValue = 0;
+            for (int i = 0; i < NUM_SAMPLES; i++) {
+                soundReadValue = soundSensor.getData();
+                if (soundReadValue > SOUND_TRIGGER_VALUE) {
+                    // std::cout << soundReadValue << '\n';
+                    reachTriggerValue = 1;
+                }
+            }
+            if (reachTriggerValue) {
+                slapCount++;
+                if (slapCount == 1) begin = Time::now();
+                if (slapCount == 2) {
+                    auto end = Time::now();
+                    fsec fs = end - begin;
+                    ms elapse = std::chrono::duration_cast<ms>(fs);
+                    if (elapse.count() < TIME_INTERVAL_ms ) {
+                        // TODO: toggle relay
+						soundRelayActivation = (soundRelayActivation == 1) ? 0 : 1;
+						// sleep(5);
+                        break;
+                    } else {
+                        // reset slapCount to 1
+                        slapCount = 1;
+                        begin = end;
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
 	cout << "MAIN <------" << endl;
 	IRDistanceSensor irSensor = IRDistanceSensor();
+
+	// start soundSensor thread
+	thread soundSensor (driveByClappingWithSoundSensor);
 //
 //	//TODO: this should be moved to the main() / binary for the irSensor.
 	irSensor.serveData();
---
+++ beaglebone_green/src/sensors/sound_sensor.cpp
@@ -0,0 +1,33 @@
+/*
+ * ir_distance_sensor.cpp
+ *
+ *  Created on: Jul 7, 2018
+ *      Author: tudatn
+ */
+
+#include "sensors/sound_sensor.h"
+#include <string>
+#include <sstream>
+#include <unistd.h>
+
+#include <iostream>
+
+using namespace std;
+
+SoundSensor::SoundSensor()
+{
+	soundAIN = 4; // default AIN4 P9_GPIO33
+	soundADC = ADC(soundAIN);
+}
+
+SoundSensor::SoundSensor(int AINNumber)
+{
+	soundAIN = AINNumber;
+	soundADC = ADC(soundAIN);
+}
+
+int SoundSensor::getData()
+{
+	data = soundADC.readValue();
+	return data;
+}
